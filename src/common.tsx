7390fa6d41fe5252de0ed469add375b82536d448
Kenneth <8515878+kenesu-h@users.noreply.github.com>
2022-04-19 19:13:25 -0400

    Some rudimentary TypeScript stuff.

---
+++ src/common.tsx
@@ -0,0 +1,15 @@
+class Shipment {
+  private id: number;
+  private tradeType: String;
+  private commodityId: number;
+  private transportMethodId: number;
+  private source: number;
+  private destination: number;
+  private value: number;
+  private weight: number;
+  private freightCharges: number;
+  private goodType: String;
+  private containerized: bool;
+  private month: number;
+  private year: number;
+}
